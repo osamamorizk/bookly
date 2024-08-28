deb42bb85640bb40833c2328c7cd180cbe52c18d
osamamorizk <osamarizk262@gmail.com>
2024-08-28 20:46:58 +0300

    create featured books cubit

---
+++ lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model/book.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit(this.homeRepo) : super(FeturedBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> featchFeaturedBooks() async {
+    var result = await homeRepo.featchFuturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(errorMessage: failure.errorMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books: books));
+    });
+  }
+}
--- lib/features/home/presentation/manger/fetured_books_cubit/fetured_books_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:bookly/features/home/data/models/book_model/book.dart';
-import 'package:equatable/equatable.dart';
-
-part 'fetured_books_state.dart';
-
-class FeturedBooksCubit extends Cubit<FeturedBooksState> {
-  FeturedBooksCubit() : super(FeturedBooksInitial());
-}
