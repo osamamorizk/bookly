d737b5ec74db713ec9090208d66f31f69ea5a44d
osamamorizk <osamarizk262@gmail.com>
2024-08-28 16:15:38 +0300

    add failure class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/models/book_model/book.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  featchFuturedBooks();
-  featchBestSellerBooks();
+  Future<Either<Failure, List<Book>>> featchFuturedBooks();
+  Future<Either<Failure, List<Book>>> featchBestSellerBooks();
 }
