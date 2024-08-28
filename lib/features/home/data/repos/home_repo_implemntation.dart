ac8f9093b083923f61604b788c3bf469497df3a3
osamamorizk <osamarizk262@gmail.com>
2024-08-28 19:14:14 +0300

    implement get request and dio exception

--- lib/features/home/data/repos/home_repo_implemntation.dart
+++ lib/features/home/data/repos/home_repo_implemntation.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -20,7 +21,15 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(
+          ServerFailure(
+            errorMessage: e.toString(),
+          ),
+        );
+      }
     }
   }
 
